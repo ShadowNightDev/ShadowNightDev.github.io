7fae532bb0833583de18fe3ca22231478b0ce159
K <tjaicenhong2@gmail.com>
2026-04-09 13:08:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  GAS_PROXY_URL: "https://script.google.com/macros/s/AKfycbwcH1pa1gD2ergikKNPlEOPKVsr1q_nGn-KZ4RM5tLpbCGFnxFayVUhjVvSUcEXOC0R/exec",
+  API_KEY: "sonix_2026_secure_x9A2"
+};
